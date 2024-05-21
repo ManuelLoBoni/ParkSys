2e125fa7e376c2c10d7c69045feebed233978fd6
ManuelLoBo <lopezbonillajuan@gmail.com>
2024-05-20 20:47:28 -0600

    Update raspServer.py

--- raspServer.py
+++ raspServer.py
@@ -8,11 +8,12 @@ import threading
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-# Configuración del cliente
-HOST = 'localhost' # Dirección IP del servidor
-PORT = 65432 # Puerto para la comunicación
-PORT = 65433
 reader = SimpleMFRC522()
+
+# Configuración del cliente
+HOST = '192.168.123.147' # Dirección IP del servidor
+PORT = 65432 # Puerto para la comunicación
+PORT2 = 65433
 
 # Ruta de la imagen a enviar
 imagen_a_enviar = 'image/placa.jpg'
@@ -38,7 +39,7 @@ def sendServer(image):
 def sendText(message):
     # Conexión al servidor
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((HOST, PORT))
+        client_socket.connect((HOST, PORT2))
 
         # Envío del mensaje al servidor
         client_socket.sendall(message.encode())
@@ -47,11 +48,12 @@ def sendText(message):
 
 # Configurando variables
 picam2 = Picamera2()
+picam2.start()
 
 #Configurando Sensor Ultrasónico
-GPIO.setmode(GPIO.BCM)
-TRIG_PIN = 23
-ECHO_PIN = 24
+GPIO.setmode(GPIO.BOARD)
+TRIG_PIN = 16
+ECHO_PIN = 18
 GPIO.setup(TRIG_PIN, GPIO.OUT)
 GPIO.setup(ECHO_PIN, GPIO.IN)
     
@@ -76,22 +78,30 @@ def medir_distancia():
 
 def tomarFoto():
     global imagen_a_enviar
-    picam2.start_and_capture_file(imagen_a_enviar)
+    picam2.capture_file(imagen_a_enviar)
+    print("Imagene tomada")
+    # picam2.start_and_capture_file(imagen_a_enviar)
 
-def previewCamera():
-    picam2.preview_configuration()
+def rifas():
+    while True:
+        id, nombreCajon = reader.read()
+        id_hex_tarjeta = format(id, 'X')  # Convertimos el ID a hexadecimal
+        print(f"ID de la tarjeta: {id_hex_tarjeta}")
+        print(f"Nombre del cajon: {nombreCajon}")
+        sendText(id_hex_tarjeta)
+        time.sleep(0.5)
 
-threading.Thread(target=previewCamera).start()
+threading.Thread(target=rifas).start()
 
 
-
-while True:
-    medida = medir_distancia()
-    if medida > 100:
-        tomarFoto()
-        sendServer(imagen_a_enviar)
-
-    id, nombreCajon = reader.read()
-    id_hex_tarjeta = format(id, 'X')  # Convertimos el ID a hexadecimal
-    print(f"ID de la tarjeta: {id_hex_tarjeta}")
-    print(f"Nombre del cajon: {nombreCajon}")
+try:
+    while True:
+        medida = medir_distancia()
+        print(f"Distancia: {medida} cm")
+        if(medida < 50):{
+            tomarFoto(),
+            sendServer(imagen_a_enviar)
+        }
+        time.sleep(1)
+except Exception as e:
+    print(e)
